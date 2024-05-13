762584c4b550bb90a4081d3c685169d745fe4401
ImonPanda <153349236+ImonPanda@users.noreply.github.com>
2024-05-13 17:45:54 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,9 +1,18 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyCnJfRRM9D6j14lyl7hp8yuy6OOELW-vO8",
+    authDomain: "kwiter-b51a1.firebaseapp.com",
+    databaseURL: "https://kwiter-b51a1-default-rtdb.firebaseio.com",
+    projectId: "kwiter-b51a1",
+    storageBucket: "kwiter-b51a1.appspot.com",
+    messagingSenderId: "612133248919",
+    appId: "1:612133248919:web:7f800c3d612c8d059a2ed2"
+  };
 
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
 
 
 function addUser()
